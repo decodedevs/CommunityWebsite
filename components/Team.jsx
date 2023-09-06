1b099ce1174804d07e8bdedbc9030306c1f23147
anshuman <anshumannarendrajitsingh@gmail.com>
2023-09-06 13:37:25 +0530

    added portfolio websites

--- components/Team.jsx
+++ components/Team.jsx
@@ -176,7 +176,7 @@ export default function Team() {
                 </a>
                 {
                   person.social.website ?
-                    <a className="text-black hover:text-slate-700">
+                    <a href={person.social.website} className="text-black hover:text-slate-700">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6 ml-2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                       </svg>
